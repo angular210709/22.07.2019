088601a243687e0619196ff986fda88082631813
angular210709 <53136440+angular210709@users.noreply.github.com>
2019-07-22 10:33:19 +0300

    Add files via upload

---
+++ Targil1/index.js
@@ -0,0 +1,8 @@
+function clicked()
+{
+    //alert(document.getElementById("nameTxt").value)
+    //call new Person with values from input boxes
+    //-- create Person
+    //console.log(person)
+    // -- use class
+}
---
+++ Targil1/person.js
@@ -0,0 +1,6 @@
+function Person(name, age, password)
+{
+    this.name = name
+    this.age = age
+    this.password = password
+}
